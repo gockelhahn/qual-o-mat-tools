9383a8060d9735a6848871c2a428497f79eac3b1
Felix Bolte <bolte.felix@gmail.com>
2022-04-03 14:01:00 +0200

    add schleswigholstein 2022

--- tools/wahlomat_2021.py
+++ tools/wahlomat_2021.py
@@ -24,7 +24,8 @@ wom = [
     'https://archiv.wahl-o-mat.de/berlin2021/',
     'https://archiv.wahl-o-mat.de/mecklenburgvorpommern2021/',
     'https://archiv.wahl-o-mat.de/bundestagswahl2021/',
-    'https://www.wahl-o-mat.de/saarland2022/'
+    'https://www.wahl-o-mat.de/saarland2022/',
+    'https://www.wahl-o-mat.de/schleswigholstein2022/'
 ]
 
 
